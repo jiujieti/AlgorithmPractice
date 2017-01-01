94ba3684e87181a555377186a607a2b05559b9ac
mengqi <melody2014ymq@gmail.com>
2017-01-01 17:12:46 +0100

    Test cases added.

--- sort/heapsort.py
+++ sort/heapsort.py
@@ -1,3 +1,6 @@
+import random
+import sys
+
 def build_max_heap_property(A, heapsize, i):
   largest = i
   left = 2*i + 1
@@ -23,5 +26,16 @@ def heapsort(A):
     build_max_heap_property(A, heapsize, 0)
   return A
 
-A = [2, 5, 3, 1, 4]
-print (str(heapsort(A)))
+for i in range(0, 5):
+  max_num = random.randint(0, 100000)
+  num = random.randint(0, 10000)
+  A = random.sample(range(max_num), num)
+  A_sorted = heapsort(A)
+  if A_sorted == sorted(A):
+    print ("The original list is {0}".format(str(A)))
+    print ("The heap-sorted list is {0}".format(str(A_sorted)))
+    print ("The heapsort algorithm is correct!")
+  else:
+    print ("The original list is {0}".format(str(A)))
+    print ("The heap-sorted list is {0}".format(str(A_sorted)))
+    print ("The heapsort algorithm is wrong.")
