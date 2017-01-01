302acd0b39b48149e815c42c54132e69e7256ecb
mengqi <melody2014ymq@gmail.com>
2017-01-01 16:48:55 +0100

    Adjust heapsize implementation.

--- sort/heapsort.py
+++ sort/heapsort.py
@@ -2,9 +2,9 @@ def build_max_heap_property(A, heapsize, i):
   largest = i
   left = 2*i + 1
   right = 2*i + 2
-  if left <= heapsize and A[left] > A[largest]:
+  if left < heapsize and A[left] > A[largest]:
     largest = left
-  if right <= heapsize and A[right] > A[largest]:
+  if right < heapsize and A[right] > A[largest]:
     largest = right
   if largest != i:
     A[i], A[largest] = A[largest], A[i]
@@ -12,14 +12,16 @@ def build_max_heap_property(A, heapsize, i):
 
 def build_max_heap(A):
   for i in range(len(A) // 2, -1, -1):
-    build_max_heap_property(A, len(A) - 1, i)
+    build_max_heap_property(A, len(A), i)
     
 def heapsort(A):
   build_max_heap(A)
-  heapsize = len(A) - 1
+  heapsize = len(A)
   for i in range(len(A)-1, 0, -1):
     A[i], A[0] = A[0], A[i]
     heapsize -= 1
     build_max_heap_property(A, heapsize, 0)
   return A
 
+A = [2, 5, 3, 1, 4]
+print (str(heapsort(A)))
