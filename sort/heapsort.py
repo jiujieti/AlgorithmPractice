5386f5c9bc0c98af55f45425da67054d3f7482d2
mengqi <melody2014ymq@gmail.com>
2017-01-01 14:37:33 +0100

    Add sort directory. Add algorithm of heapsort.

---
+++ sort/heapsort.py
@@ -0,0 +1,25 @@
+def build_max_heap_property(A, heapsize, i):
+  largest = i
+  left = 2*i + 1
+  right = 2*i + 2
+  if left <= heapsize and A[left] > A[largest]:
+    largest = left
+  if right <= heapsize and A[right] > A[largest]:
+    largest = right
+  if largest != i:
+    A[i], A[largest] = A[largest], A[i]
+    build_max_heap_property(A, heapsize, largest)  
+
+def build_max_heap(A):
+  for i in range(len(A) // 2, -1, -1):
+    build_max_heap_property(A, len(A) - 1, i)
+    
+def heapsort(A):
+  build_max_heap(A)
+  heapsize = len(A) - 1
+  for i in range(len(A)-1, 0, -1):
+    A[i], A[0] = A[0], A[i]
+    heapsize -= 1
+    build_max_heap_property(A, heapsize, 0)
+  return A
+
