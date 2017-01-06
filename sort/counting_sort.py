ce7c9300f045a755a3e37a53c3dd4ca4de8ceee4
Mengqi Yang <mengqi@MengqideMacBook-Pro.local>
2017-01-06 21:40:05 +0100

    Tests for counting sort added.

--- sort/counting_sort.py
+++ sort/counting_sort.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def counting_sort(li, k):
   c = [0] * (k+1)
   for j in range(0, len(li)):
@@ -10,3 +12,18 @@ def counting_sort(li, k):
     c[li[j]] -= 1
   return res
 
+class MyException(Exception):
+  pass
+
+test_cases = []
+for i in range(0, 5):
+  li = [randint(0, 100) for a in range(0, randint(0, 1000))]
+  test_cases.append(li)
+test_cases.append([])
+
+for li in test_cases:
+  if counting_sort(li, 100) == sorted(li):
+    print ("The couting sort algorithm is correct!")  
+  else:
+    raise MyException("The counting sort algorithm is incorrect!")
+    
